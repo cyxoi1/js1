241cad541e745c6563161a6bc38be4b3f6e72cd3
cyxoi1 <cyxoi1@yandex.ru>
2020-05-24 09:49:51 +0100

    update

--- helpers.js
+++ helpers.js
@@ -9,5 +9,148 @@ function mult(a, b){
 const arr[];
 
 
-module.exports = {add, mult};
+//1
+
+function season(month){
+    let res;
+    switch (month) {
+        case 12:
+        case 1:
+        case 2: res = 'Winter';
+            break;
+        case 3:
+        case 4:
+        case 5: res = 'Spring';
+            break;
+        case 6:
+        case 7:
+        case 8: res = 'Summer';
+            break;
+        case 9:
+        case 10:
+        case 11: res = 'Autumn';
+            break;
+        default: res = 'Wrong month';
+    }
+    return res;
+}
+
+//2
+
+function countOfDivisors(n){
+    let count = 0;
+    for (let i = 2; i < (n / 2) + 1; i++){
+        if (n % i === 0){
+            count++
+        }
+    }
+    return count;
+}
+
+//3
+function isPrime(num){
+    return countOfDivisors(num) === 0;
+}
+//4
+
+function primeNumbers(n){
+    let res = [];
+    for (let i = 2; i <= n; i++){
+        if (isPrime(i)) res.push(i)
+    }
+    return res;
+}
+
+//5
+
+function countOfPrimeNumbers(n, m){
+    let count = 0;
+    for (let i = n; i <= m; i++){
+        if (isPrime(i)) {
+            count++;
+        }
+    }
+    return count;
+}
+ //6
+
+function removeSpaces(str){
+    let res = '';
+    for (let i = 0; i < str.length; i++){
+        if (str[i] !== ' '){
+            res = res + str[i]
+        }
+    }
+    return res;
+}
+
+//7
+
+function removeSpacesR(str){
+    return str.replace(/ /g, '')
+}
+
+//8
+
+function isPalindrome(str){
+    str = str.toLowerCase();
+    str = removeSpaces(str)
+    let reverseStr = '';
+    for (let i = 0; i < str.length; i++){
+        reverseStr = str[i] + reverseStr;
+    }
+    return str === reverseStr;
+}
+
+//9
+
+function countPalindromes(arr){
+    let count = 0;
+    for (let i = 0; i < arr.length; i++){
+        if (isPalindrome(arr[i])){
+            count++
+        }
+    }
+    return count;
+}
+
+//10
+
+function isVowel(c){
+    c = c.toLowerCase();
+    let vowels = 'aeuio';
+    for (let i = 0; i < vowels.length; i++){
+        if (c == vowels[i]) return true;
+    }
+    return false
+}
+
+//=============================================
+
+function avg(arr = []) {
+    // should return avg 0 for empty array
+    if (!Array.isArray(arr)) return 0;
+    // should return avg 0 for empty array
+    if (arr.length === 0) return 0;
+    // avg calculation
+    //
+    let sum = 0, el = 0; //
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] !== 'number') continue;
+        el++; //
+        sum += arr[i];
+    }
+    // should return 0 for all non numbers
+    if (el === 0) return 0;
+    return sum / el;
+    // return arr.reduce((acc, cur) => acc + cur, 0) / arr.length;
+}
+//module.exports = { avg };
+
+
+
+module.exports = {add, mult, season, countOfDivisors,
+    isPrime, primeNumbers,
+    countOfPrimeNumbers, removeSpaces, removeSpacesR,
+    isPalindrome, countPalindromes, isVowel, avg};
 //and wha s next?
--- test/index.js
+++ test/index.js
@@ -1,16 +1,112 @@
-const {add, mult} = require('../helpers');
-const chai = require('chai');
-const expect = chai.expect;
+// const {add, mult, season, countOfDivisors,
+// isPrime, primeNumbers} = require('../helpers');
+// const chai = require('chai');
+// const expect = chai.expect;
+
+describe('Function add & mult', () => {
+
+ it('should function add 2 5', () => {
+  expect(add(2, 5)).equal(7)
+ });
+
+ it('should function add -2 -5', () => {
+  expect(add(-2, -5)).equal(-7)
+ });
+
+ it('should function mult 2 5', () => {
+  expect(mult(2, 5)).equal(10)
+ })
+})
 
 
-it('should function add 2 5', () => {
- expect(add(2,5)).equal(7)
+
+
+
+//========================================================
+//1
+// it('should season ', function () {
+//    expect(season(5)).equal('Spring')
+// });
+// //2
+// it('should countOfDivisors ', function () {
+//  expect(countOfDivisors(18)).equal(4)
+// });
+// //3
+// it('should isPrime ', function () {
+//  expect(isPrime(5)).equal(true)
+// });
+// //4
+// it('should primeNumbers ', function () {
+//  expect(primeNumbers(10)).deep.equal([2, 3, 5, 7])
+// })
+// //5
+//
+// it('should countOfPrimeNumbers ', function () {
+//  expect(countOfPrimeNumbers(10, 90)).equal(20)
+// })
+//
+// //6
+//
+// it('should removeSpaces ', function () {
+//  expect(removeSpaces('a b c')).equal('abc')
+// })
+//
+// //7
+//
+// it('should removeSpacesR ', function () {
+//  expect(removeSpacesR('a b   c')).equal('abc')
+// })
+//
+// //8
+//
+// it('should isPalindrome ', function () {
+//  expect(isPalindrome("Dia d")).equal(false)
+// })
+//
+// //9
+//
+// it('should countPalindromes ', function () {
+//  expect(countPalindromes(["noon", "nice", "Bob", "Pull up if i pull up", "good"])).equal(3)
+// })
+//
+// //10
+//
+// it('should isVowel ', function () {
+//  expect(isVowel('o')).equal(true)
+// })
+//=========================================
+
+
+//const { avg } = require('../helpers');
+const expect = require('chai').expect;
+describe('Function AVG', () => {
+ it('should calculate avg for positive numbers', () => {
+  expect(avg([1, 2, 3])).eq(2);
+  expect(avg([10, 20, 30])).eq(20);
+ });
+ it('should return avg 0 for empty array', () => {
+  expect(avg([])).eq(0);
+ });
+ it('should return avg 0 without arguments', () => {
+  expect(avg()).eq(0);
+ });
+ it('should return avg 0 if arg is not an array', () => {
+  expect(avg({})).eq(0);
+  expect(avg(123)).eq(0);
+  expect(avg('aaa')).eq(0);
+  expect(avg(null)).eq(0);
+ });
+ it('should ignore non numbers in avg calculation', () => {
+  expect(avg([1, 2, 'a', 3])).eq(2);
+  expect(avg(['a', 3])).eq(3);
+ });
+ it('should return 0 for all non numbers', () => {
+  expect(avg(['a', '3', 'qwe'])).eq(0);
+ });
 });
 
-it('should function add -2 -5', () => {
- expect(add(-2,-5)).equal(-7)
-});
 
-it('should function mult 2 5', () => {
- expect(mult(2,5)).equal(10)
-});
+const {add, mult, season, countOfDivisors,
+ isPrime, primeNumbers, countOfPrimeNumbers,
+ removeSpaces, removeSpacesR, isPalindrome,
+ countPalindromes, isVowel, avg} = require('../helpers');
